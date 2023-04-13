d2dc6087d14a630819d3043ec203103474e483f9
José Domingo Muñoz <josedom24@gmail.com>
2023-04-13 17:57:51 +0200

    Flash app

---
+++ app/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, abort, redirect, request
+import datetime
+import socket
+import os
+
+app = Flask(__name__)	
+
+@app.route('/',methods=["GET","POST"])
+def inicio():
+    hoy=datetime.datetime.now()
+    info=os.environ["INFORMACION"]
+    return render_template("inicio.html",hoy=hoy,server=socket.gethostname(),info=info)
+
+
+
+app.run('0.0.0.0',8080,debug=True)
