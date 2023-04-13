d2b03f152d42e6e039b53592f3d8e11b5486125e
José Domingo Muñoz <josedom24@gmail.com>
2023-04-13 21:02:43 +0200

    App temperaturas

--- app/app.py
+++ app/app.py
@@ -1,16 +1,27 @@
-from flask import Flask, render_template, abort, redirect, request
-import datetime
-import socket
-import os
-
+from flask import Flask, request,url_for,render_template,abort
+from lxml import etree
+import requests
 app = Flask(__name__)	
 
 @app.route('/',methods=["GET","POST"])
 def inicio():
-    hoy=datetime.datetime.now()
-    info=os.environ["INFORMACION"]
-    return render_template("inicio.html",hoy=hoy,server=socket.gethostname(),info=info)
+	doc=etree.parse("sevilla.xml")
+	municipios=doc.findall("municipio")
+	return render_template("inicio.html",municipios=municipios)
 
+@app.route('/<code>')
+def temperatura(code):
+	#try:
+	response = requests.get("http://www.aemet.es/xml/municipios/localidad_"+code+".xml")
+	xml = response.content
+	doc=etree.fromstring(xml)
 
+	#except:
+	#	abort(404)
+	name=doc.find("nombre").text
+	max=doc.find("prediccion/dia/temperatura").find("maxima").text
+	min=doc.find("prediccion/dia/temperatura").find("minima").text
+	return render_template("temperaturas.html",name=name,max=max,min=min)
 
-app.run('0.0.0.0',8000,debug=True)
+if __name__ == '__main__':
+	app.run('0.0.0.0',8080,debug=True)
